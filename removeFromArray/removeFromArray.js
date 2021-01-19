7dfb5378cd67967ed279ffb5f5d6ea8ace957c2b
harmolipi <nikob381@gmail.com>
2021-01-19 12:58:59 -0500

    Finished removeFromArray exercise

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (inputArray, ...removeArgs) {
+    let newArray = inputArray;
 
+    for (x = 0; x < removeArgs.length; x++) {
+        for (y = 0; y < newArray.length; y++) {
+            if (newArray[y] === removeArgs[x]) {
+                newArray.splice(y, 1);
+            }
+        }
+    }
+    return newArray;
 }
 
 module.exports = removeFromArray
