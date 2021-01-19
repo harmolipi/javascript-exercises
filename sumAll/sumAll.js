d746bbf8474bd6193e8858ff71c154970388296a
harmolipi <nikob381@gmail.com>
2021-01-19 13:26:28 -0500

    Finished sumAll exercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function (x, y) {
+    if (typeof x != `number` || typeof y != `number` || Math.sign(x) === -1 || Math.sign(y) === -1) {
+        return `ERROR`;
+    }
 
+    let bigger;
+    let smaller;
+    let sum = 0;
+    let sum2 = 0;
+
+    if (x > y) {
+        bigger = x;
+        smaller = y;
+    } else if (y > x) {
+        bigger = y;
+        smaller = x;
+    } else {
+        return x;
+    }
+
+    for (i = smaller; i <= bigger; i++) {
+        sum += i;
+    }
+
+    return sum;
 }
 
 module.exports = sumAll
