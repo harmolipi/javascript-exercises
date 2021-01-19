95f38e1f8d5e20c50e205f6ca2820f974eaaa7b6
harmolipi <nikob381@gmail.com>
2021-01-19 12:33:55 -0500

    Finished reverseString exercise

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (stringToReverse) {
+    let finishedString = ``;
+    
+    for (i = 1; i <= stringToReverse.length; i++) {
+        finishedString += stringToReverse.substr(i * -1, 1);
+    }
+    
+    return finishedString;
 }
 
 module.exports = reverseString
