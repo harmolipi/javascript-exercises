035da944b75d87f88fa9ccc34527b63da3943098
harmolipi <nikob381@gmail.com>
2021-01-19 02:38:57 -0500

    Finished helloWorld and repeatString exercises

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat, num) {
+    let newString = ``;
 
+    if (num < 0) {
+        newString = `ERROR`;
+        return newString;
+    }
+
+    for (i=0; i < num; i++) {
+        newString += stringToRepeat;
+    }
+    return newString;
 }
 
 module.exports = repeatString
