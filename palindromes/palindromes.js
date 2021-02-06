3cf13ad7b110f6c85f391185e04254a450801129
harmolipi <nikob381@gmail.com>
2021-02-06 01:45:43 -0500

    Finished palindromes exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function() {
+const palindromes = function (myString) {
+    let simplified = myString
+        .toLowerCase()
+        .replace(/[.,!*+\-?^${} ()|[\]\\]/g, '');
+    let reverseString = string => {
+        newString = string[string.length - 1];
+        for (i = string.length - 2; i >= 0; i--) {
+            newString += string[i];
+        }
+        return newString;
+    }
 
+
+    return simplified === reverseString(simplified);
 }
 
 module.exports = palindromes
