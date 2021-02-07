e3064a18b63b7e9c04046c871b1614238266d1d1
harmolipi <nikob381@gmail.com>
2021-02-07 18:30:22 -0500

    Finished the Caesar exercise (and barely survived)

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,29 @@
-const caesar = function() {
+const caesar = function (str, shift = 0) {
+    let shiftedString = ``;
+    let shiftedCode;
 
+    if (shift % 26 === 0) return str;
+
+    for (i = 0; i < str.length; i++) {
+        let isUppercase = (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90);
+        let isLowercase = (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122);
+        let adjustedShift = shift % 26;
+
+        if (isUppercase || isLowercase) {
+            shiftedCode = str.charCodeAt(i) + adjustedShift;
+
+            if ((isUppercase && shiftedCode > 90) || (isLowercase && shiftedCode > 122)) {
+                shiftedCode -= 26;
+            } else if ((isUppercase && shiftedCode < 65) || (isLowercase && shiftedCode < 97)) {
+                shiftedCode += 26;
+            }
+        } else {
+            shiftedCode = str.charCodeAt(i);
+        }
+
+        shiftedString += String.fromCharCode(shiftedCode);
+    }
+    return shiftedString;
 }
 
 module.exports = caesar
