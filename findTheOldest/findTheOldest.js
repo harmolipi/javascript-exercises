9ede680487a670866078e7a6b0f79c9c725bfe2e
harmolipi <nikob381@gmail.com>
2021-02-08 09:35:25 -0500

    Finished findTheOldest exercise

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-let findTheOldest = function() {
+let findTheOldest = function (people) {
+    return (people.reduce(ages));
+}
 
+let ages = function (oldestPerson, currentPerson) {
+    let oldestYear;
+    let currentYear;
+    let date = new Date().getFullYear();
+    oldestPerson.yearOfDeath !== undefined ? oldestYear = oldestPerson.yearOfDeath : oldestYear = date;
+    currentPerson.yearOfDeath !== undefined ? currentYear = currentPerson.yearOfDeath : currentYear = date;
+    return (oldestYear - oldestPerson.yearOfBirth) > (currentYear - currentPerson.yearOfBirth) ? oldestPerson : currentPerson;
 }
 
 module.exports = findTheOldest
