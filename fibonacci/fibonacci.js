2d24b73d3ab345881e980680136b69e55b0d4c6f
harmolipi <nikob381@gmail.com>
2021-02-07 19:00:56 -0500

    Finished the fibonacci exercise

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function (int) {
+    let previousFirst = 0;
+    let previousSecond = 1;
+    let fib = [1];
+    int = checkNum(int);
+    if (int === "OOPS") return int;
 
+    for (i = 1; i < int; i++) {
+        fib[i] = previousFirst + previousSecond;
+        previousFirst = previousSecond;
+        previousSecond = fib[i];
+    }
+
+    return fib[i - 1];
+}
+
+const checkNum = function (num) {
+    num = Number(num);
+    if (num <= 0 || isNaN(num)) {
+        return "OOPS";
+    }
+    return num;
 }
 
 module.exports = fibonacci
